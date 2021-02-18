d2ead9883d64fa631120808b6f2bb92079166045
dhmm <dhmm@outlook.com>
2021-02-18 15:31:55 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'materialize-css';
+import { Navbar, NavItem, Icon, Button } from 'react-materialize';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar
+                alignLinks="right"
+                brand={<a className="brand-logo" href="/">BS</a>}
+                centerChildren
+                className="light-blue lighten-1"
+                id="mobile-nav"
+                menuIcon={<Icon>menu</Icon>}
+                options={{
+                    draggable: true,
+                    edge: 'left',
+                    inDuration: 250,
+                    onCloseEnd: null,
+                    onCloseStart: null,
+                    onOpenEnd: null,
+                    onOpenStart: null,
+                    outDuration: 200,
+                    preventScrolling: true
+                }}
+            >
+                <NavItem href="/login">
+                    Login
+                </NavItem>
+                <NavItem href="/register">
+                    Register
+                </NavItem>
+            </Navbar>
+            
+            <Switch>    
+                
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/view">
+                    <View />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
+function Home() {
+    return (
+        <div>
+            <div class="section no-pad-bot" id="index-banner">
+                <div class="container">
+                    <br /><br />
+                    <h1 class="header center orange-text">Bid system</h1>
+                    <div class="row center">
+                        <h5 class="header col s12 light">Demo bidding system by dhmm</h5>
+                        <p>You can login, register and create items for bidding. Also you can bid to existing items.</p>
+                    </div>
+                    <div class="row center">
+                        <Button
+                            href="#"
+                            node="button"
+                            className="btn-large waves-effect waves-light orange"
+                            style={{ marginRight: '5px' }}
+                            waves="light"
+                        >
+                            How it works ?
+                        </Button>
+                        <Button
+                            node="button"
+                            className="btn-large waves-effect waves-light purple"
+                            style={{ marginRight: '5px' }}
+                            waves="light"
+                        >
+                            View items
+                        </Button>
+                        <Button
+                            node="button"
+                            className="btn-large waves-effect waves-light blue"
+                            style={{ marginRight: '5px' }}
+                            waves="light"
+                        >
+                            Register
+                        </Button>
+                        <Button
+                            node="button"
+                            className="btn-large waves-effect waves-light green"
+                            style={{ marginRight: '5px' }}
+                            waves="light"
+                        >
+                            Login
+                        </Button>
+                    </div>
+                    <br /><br />
+
+
+
+                </div>
+            </div>
+        </div>
+    );
+}
+function Register() {
+    return (
+        <div>Register</div>
+    );
+}
+function Login() {
+    return (
+        <div>Login</div>
+    );
+}
+function View() {
+    return (
+        <div>View</div>
+    );
+}
 export default App;
