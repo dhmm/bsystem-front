c0f7e6393c98369b04cb74aeeb15563d9c16a23f
dhmm <dhmm@outlook.com>
2021-03-01 16:20:09 +0200

    registration completed

--- src/home/RegisterModal.js
+++ src/home/RegisterModal.js
@@ -4,14 +4,15 @@ import axios from 'axios';
 
 class RegisterModal extends React.Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         this.state = {
             fullname: "",
             username: "",
             email: "",
-            password: ""
+            password: "",
+            message: "",            
+            hasError: false
         }
 
         this.fullNameChanged = this.fullNameChanged.bind(this);
@@ -22,38 +23,45 @@ class RegisterModal extends React.Component {
 
     fullNameChanged(event) {
         this.setState({
-            fullname : event.target.value
+            fullname: event.target.value
         });
     }
     usernameChanged(event) {
         this.setState({
-            username : event.target.value
+            username: event.target.value
         });
     }
     emailChanged(event) {
         this.setState({
-            email : event.target.value
+            email: event.target.value
         });
     }
     passwordChanged(event) {
         this.setState({
-            password : event.target.value
+            password: event.target.value
         });
     }
 
-    register(){
-        axios.post('https://localhost:8000/user/register', {
+
+    register() {
+        axios.post('http://localhost:8000/user/register', {
             fullname: this.state.fullname,
             username: this.state.username,
             email: this.state.email,
             password: this.state.password
-          })
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+        })
+            .then(response => {                
+                this.setState({
+                    message: response.data.message,
+                    hasError: response.data.error
+                })                
+                if (this.state.hasError === false) {
+                    
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
@@ -61,7 +69,7 @@ class RegisterModal extends React.Component {
             <Modal
                 actions={[
                     <Button flat modal="close" node="button">Close</Button>,
-                    <Button node="button"  onClick={() => { this.register(); }}>Register</Button>
+                    <Button node="button" onClick={() => { this.register(); }}>Register</Button>
                 ]}
                 bottomSheet={false}
                 fixedFooter={false}
@@ -92,30 +100,32 @@ class RegisterModal extends React.Component {
                     Register
                                 </Button>}
             >
-                
-                    <div className="row">
-                        <form className="col s12">
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="fullname" type="text" className="validate" value={this.state.fullname} onChange={this.fullNameChanged} />
-                                    <label htmlFor="fullname">Fullname</label>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input id="username" type="text" className="validate" value={this.state.username} onChange={this.usernameChanged} />
-                                    <label htmlFor="username">Username</label>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate" value={this.state.email} onChange={this.emailChanged}/>
-                                    <label htmlFor="email">Email</label>
-                                </div>
-                                <div className="input-field col s12">
-                                    <input id="password" type="password" className="validate" value={this.state.password} onChange={this.passwordChanged} />
-                                    <label htmlFor="password">Password</label>
-                                </div>
+
+                <div className="row">
+                    <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="fullname" type="text" className="validate" value={this.state.fullname} onChange={this.fullNameChanged} />
+                                <label htmlFor="fullname">Fullname</label>
                             </div>
-                        </form>
-                    </div>
-                
+                            <div className="input-field col s12">
+                                <input id="username" type="text" className="validate" value={this.state.username} onChange={this.usernameChanged} />
+                                <label htmlFor="username">Username</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input id="email" type="email" className="validate" value={this.state.email} onChange={this.emailChanged} />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input id="password" type="password" className="validate" value={this.state.password} onChange={this.passwordChanged} />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </form>
+                    <div style={{ color: this.state.hasError === true ? 'red' : 'green' }}>{this.state.message}</div>
+
+                </div>
+
             </Modal>
         )
     }
